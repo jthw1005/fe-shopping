f8c2d91d2f1f62c0f8a2a607724fd08f85aa8e7a
jthw1005 <skl0105@naver.com>
2022-03-21 18:15:11 +0900

    Feat: Add event handler for drop down menu off

--- public/js/headerDropDownMenu.js
+++ public/js/headerDropDownMenu.js
@@ -1,17 +1,30 @@
 function setSearchCategoryEvent() {
   const categoryBtn = document.querySelector(".search__category");
+  const categoryBtnItem = document.querySelector(".search__category-list");
+
   categoryBtn.addEventListener("click", handleDropDownMenu);
+  categoryBtnItem.addEventListener("mouseleave", handleDropDownMenuOff);
+  categoryBtnItem.addEventListener("click", handleCategorySelection);
 }
 
 function handleDropDownMenu(event) {
-  const eventTarget = event.target;
-  const toggleBtn = eventTarget.querySelector("i");
+  const currentTarget = event.currentTarget;
 
+  const toggleBtn = currentTarget.querySelector("i");
   toggleBtn.classList.toggle("fa-caret-down");
   toggleBtn.classList.toggle("fa-caret-up");
 
-  const hiddenMenuList = eventTarget.parentNode.querySelector(".search__category-list");
+  const hiddenMenuList = currentTarget.nextElementSibling;
   hiddenMenuList.classList.toggle("hidden");
+}
+
+function handleDropDownMenuOff(event) {
+  event.target.classList.add("hidden");
+}
+
+function handleCategorySelection(event) {
+  event.currentTarget.classList.add("hidden");
+  document.querySelector(".category__title").innerHTML = event.target.innerHTML;
 }
 
 export default setSearchCategoryEvent;
